8344b3d4399a8c0048c77594ef93472e8f4c7bc6
zerhaz <m.stancelewski@gmail.com>
2018-06-03 19:13:53 +0200

    Initial commit

---
+++ src/main/java/zerhaz/springboot/SpringbootDatabaseApplication.java
@@ -0,0 +1,12 @@
+package zerhaz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabaseApplication.class, args);
+	}
+}
